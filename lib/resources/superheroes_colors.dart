bb6dab56a780225d89b6650f062dd158492cfdd0
Zoir I <wowdobryy@gmail.com>
2023-01-17 09:10:20 +0300

    updated superhero_card

--- lib/resources/superheroes_colors.dart
+++ lib/resources/superheroes_colors.dart
@@ -6,6 +6,7 @@ class SuperheroesColors {
   static const background = Color(0xff10141e);
   static const blue = Color(0xff00bcd4);
   static const cardBackground = Color(0xff2C3243);
+  static const indigo = Color(0xff2C3243);
   static const indigo75 = Color(0xbf2C3243);
   static const red = Color(0xff961818);
   static const grey = Color(0xff727272);
--- lib/widgets/superhero_card.dart
+++ lib/widgets/superhero_card.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:superheroes/model/alignment_info.dart';
 import 'package:superheroes/resources/superheroes_colors.dart';
 import 'package:superheroes/resources/superheroes_images.dart';
 
@@ -21,69 +22,133 @@ class SuperheroCard extends StatelessWidget {
       onTap: onTap,
       child: Container(
         height: 70.0,
+        clipBehavior: Clip.antiAlias,
         decoration: BoxDecoration(
-          color: SuperheroesColors.cardBackground,
+          borderRadius: BorderRadius.circular(8.0),
+          color: SuperheroesColors.indigo,
         ),
         child: Row(
           children: [
-            Container(
-              color: Colors.white24,
-              width: 70.0,
-              height: 70.0,
-              child: CachedNetworkImage(
-                imageUrl: superheroInfo.imageUrl,
-                width: 70.0,
-                fit: BoxFit.cover,
-                progressIndicatorBuilder: (context, url, progress) {
-                  return Container(
-                    alignment: Alignment.center,
-                    height: 24,
-                    width: 24,
-                    child: CircularProgressIndicator(
-                      color: SuperheroesColors.blue,
-                      value: progress.progress,
-                    ),
-                  );
-                },
-                errorWidget: (context, url, error) {
-                  return Center(
-                    child: Image.asset(
-                      SuperheroesImages.unknown,
-                      height: 62.0,
-                      width: 20.0,
-                      fit: BoxFit.cover,
-                    ),
-                  );
-                },
-              ),
-            ),
+            _AvatarWidget(superheroInfo: superheroInfo),
             SizedBox(width: 12.0),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    superheroInfo.name.toUpperCase(),
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  Text(
-                    superheroInfo.realName,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w400,
-                      fontSize: 14.0,
-                    ),
-                  )
-                ],
-              ),
-            ),
+            NameAndRealNameWidget(superheroInfo: superheroInfo),
+            if (superheroInfo.alignmentInfo != null)
+              AlignmentWidget(alignmentInfo: superheroInfo.alignmentInfo!),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class AlignmentWidget extends StatelessWidget {
+  final AlignmentInfo alignmentInfo;
+
+  const AlignmentWidget({
+    Key? key,
+    required this.alignmentInfo,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RotatedBox(
+      quarterTurns: 1,
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 6.0),
+        color: alignmentInfo.color,
+        alignment: Alignment.center,
+        child: Center(
+          child: Text(
+            alignmentInfo.name.toUpperCase(),
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 10,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NameAndRealNameWidget extends StatelessWidget {
+  final SuperheroInfo superheroInfo;
+
+  const NameAndRealNameWidget({
+    Key? key,
+    required this.superheroInfo,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            superheroInfo.name.toUpperCase(),
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Text(
+            superheroInfo.realName,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AvatarWidget extends StatelessWidget {
+  final SuperheroInfo superheroInfo;
+
+  const _AvatarWidget({
+    Key? key,
+    required this.superheroInfo,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white24,
+      height: 70,
+      width: 70,
+      child: CachedNetworkImage(
+        imageUrl: superheroInfo.imageUrl,
+        height: 70,
+        width: 70,
+        fit: BoxFit.cover,
+        progressIndicatorBuilder: (context, url, progress) {
+          return Container(
+            alignment: Alignment.center,
+            height: 24,
+            width: 24,
+            child: CircularProgressIndicator(
+              color: SuperheroesColors.blue,
+              value: progress.progress,
+            ),
+          );
+        },
+        errorWidget: (context, url, error) {
+          return Center(
+            child: Image.asset(
+              SuperheroesImages.unknown,
+              height: 62,
+              width: 20,
+              fit: BoxFit.cover,
+            ),
+          );
+        },
       ),
     );
   }
